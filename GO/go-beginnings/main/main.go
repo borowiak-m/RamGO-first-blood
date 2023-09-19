220004f978f3936f15be177444357653fbdb354c
Mariusz Borowiak <neutron.meme@proton.me>
2023-09-19 16:53:26 +0100

    exercise 1909

--- GO/go-beginnings/main/main.go
+++ GO/go-beginnings/main/main.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+func concat(s1, s2 string) string {
+	return s1 + s2
+}
+
+func incrementSends(sendsSoFar, sendsToAdd int) int {
+	sendsSoFar = sendsSoFar + sendsToAdd
+	return sendsSoFar
+}
+
 func main() {
-	fmt.Print("hi")
+	fmt.Println(concat("this is a message", " and this is its continuation"))
+	sendsSoFar := 430
+	const sendsToAdd = 25
+	sendsSoFar = incrementSends(sendsSoFar, sendsToAdd)
+	fmt.Println("you've sent", sendsSoFar, "messages")
+
 }
