c30bd7509ad2511d4825723777cb33dae5bfc306
Mariusz Borowiak <neutron.meme@proton.me>
2023-09-19 21:19:49 +0100

    more execrises 1909

--- GO/go-beginnings/main/main.go
+++ GO/go-beginnings/main/main.go
@@ -4,11 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"reflect"
+	"strings"
+	"time"
+	"unicode/utf8"
 )
 
 var pl = fmt.Println
+var pf = fmt.Printf
 var typeOf = reflect.TypeOf
 
 func concat(s1, s2 string) string {
@@ -26,11 +31,15 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	message, err := reader.ReadString('\n')
 
-	if err == nil {
-		pl(concat("Message received: ", message))
-	} else {
+	if err != nil {
 		log.Fatal(err)
 	}
+
+	message = strings.TrimSpace(message)
+	message = strings.Replace(message, " ", "-", -1)
+	msgLetters := strings.Split(message, "")
+	msgRunes := []rune(message)
+	pl(concat("Message received: ", message))
 
 	//break line
 	pl("==================================")
@@ -38,5 +47,57 @@ func main() {
 	sendsSoFar, sendsToAdd := 430, 35
 	sendsSoFar = incrementSends(sendsSoFar, sendsToAdd)
 	pl("Status: You've sent", sendsSoFar, "messages")
+
+	//break line
+	pl("===============Strings===========")
+
+	pl("Split :", strings.Split(message, "-"))
+	pl("Lower :", strings.ToLower(message))
+	pl("Upper :", strings.ToUpper(message))
+	pl("Rune Count :", utf8.RuneCountInString(message))
+
+	// for each
+	for i, runeVal := range message {
+		pf("%d :  %#U : %c\n", i, runeVal, runeVal)
+	}
+
+	//break line
+	pl("================ArrayAndSlice======")
+	slice1 := make([]string, 6)
+	slice2 := message[:3]
+	// for loop skipping index
+	for i, letter := range msgLetters {
+
+		if i < len(slice1) {
+			slice1[i] = letter
+		}
+
+		pl("Letter :", letter)
+	}
+
+	for _, rn := range msgRunes {
+		pl("Rune :", rn)
+	}
+
+	for _, sl := range slice1 {
+		pl("Slice char :", sl)
+	}
+
+	pl("Index 0 :", msgLetters[0])
+	pl("Array length :", len(msgLetters))
+	pl("Slice1 :", slice1, " of type :", typeOf(slice1))
+	pl("Slice2 :", slice2, " of type :", typeOf(slice2))
+	//break line
+	pl("================Time==============")
+
+	now := time.Now()
+
+	pl(now.Year(), now.Month(), now.Day())
+	pl(now.Hour(), now.Minute(), now.Second())
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	pl("Random :", r1.Intn(100))
 
 }
