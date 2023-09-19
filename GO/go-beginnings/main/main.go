4149cc6eaf6a51a27a6bad359043d88455ce8c3f
Mariusz Borowiak <neutron.meme@proton.me>
2023-09-19 19:27:04 +0100

    more execrises 1909

--- GO/go-beginnings/main/main.go
+++ GO/go-beginnings/main/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"reflect"
 )
 
+var pl = fmt.Println
+var typeOf = reflect.TypeOf
+
 func concat(s1, s2 string) string {
+	pl("Received var types: s1 as", typeOf(s1), "and s2 as", typeOf(s2))
 	return s1 + s2
 }
 
-func incrementSends(sendsSoFar, sendsToAdd int) int {
-	sendsSoFar = sendsSoFar + sendsToAdd
-	return sendsSoFar
+func incrementSends(num1, num2 int) int {
+	pl("Received var types: num1 as", typeOf(num1), "and num2 as", typeOf(num2))
+	return num1 + num2
 }
 
 func main() {
-	fmt.Println(concat("this is a message", " and this is its continuation"))
-	sendsSoFar := 430
-	const sendsToAdd = 25
+	pl("what is your message:")
+	reader := bufio.NewReader(os.Stdin)
+	message, err := reader.ReadString('\n')
+
+	if err == nil {
+		pl(concat("Message received: ", message))
+	} else {
+		log.Fatal(err)
+	}
+
+	//break line
+	pl("==================================")
+
+	sendsSoFar, sendsToAdd := 430, 35
 	sendsSoFar = incrementSends(sendsSoFar, sendsToAdd)
-	fmt.Println("you've sent", sendsSoFar, "messages")
+	pl("Status: You've sent", sendsSoFar, "messages")
 
 }
