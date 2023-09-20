c7d04ac1fa09c74f6ceed3a1a3f7c53076e28d81
Mariusz Borowiak <neutron.meme@proton.me>
2023-09-20 22:40:45 +0100

    more go exercises 20.09

--- GO/go-beginnings/main/main.go
+++ GO/go-beginnings/main/main.go
@@ -3,143 +3,52 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"sync"
+	"time"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 var typeOf = reflect.TypeOf
 
-type Aniamal interface {
-	AngrySound()
-	HappySound()
+type Account struct {
+	balance int
+	lock    sync.Mutex
 }
 
-type Cat string
-
-func (c Cat) Attack() {
-	pl("Cat attacks")
+func (a *Account) getBlanace() int {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	return a.balance
 }
 
-func (c Cat) Name() string {
-	return string(c)
+func (a *Account) withdraw(v int) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	if v > a.balance {
+		pl("Not possible to withdraw")
+	} else {
+		pf("%d withdrawn Balance : %d\n", v, a.balance)
+		a.balance -= v
+	}
 }
 
-func (c Cat) AngrySound() {
-	pl("Cat says hissssss")
+func useFunc(f func(int, int) int, x, y int) {
+	pl("Answer :", (f(x, y)))
 }
 
-func (c Cat) HappySound() {
-	pl("Cat says purrrrrrrr")
-}
-
-type customer struct {
-	name    string
-	address string
-	bal     float64
-}
-
-type rectangle struct {
-	length, height float64
-}
-
-func getCustomerInfo(c customer) {
-	pf("%s owes us %.2f\n", c.name, c.bal)
-	pf("Customer lives at : %s\n", c.address)
-}
-
-func updateCustAddress(c *customer, address string) {
-	c.address = address
-}
-
-func (c *customer) builtInAddressUpdate(address string) {
-	c.address = address
-}
-
-func (r rectangle) Area() float64 {
-	return r.length * r.height
-}
-
-type contact struct {
-	firstname, lastname, phone string
-}
-
-type business struct {
-	name, address string
-	contact
-}
-
-func (b business) info() {
-	pf("Contact for business %s is : %s %s\n", b.name, b.contact.firstname, b.contact.lastname)
+func sumVals(x, y int) int {
+	return x + y
 }
 
 func main() {
+	var acct Account
+	acct.balance = 100
+	pl("Balance :", acct.getBlanace())
+	for i := 0; i < 12; i++ {
+		go acct.withdraw(10)
+	}
+	time.Sleep(2 * time.Second)
 
-	// var myMap map [keyType]valueType
-
-	var heroes map[string]string
-	heroes = make(map[string]string)
-	pl(heroes)
-
-	villains := make(map[string]string)
-
-	heroes["Batman"] = "B Wayne"
-	heroes["Pigman"] = "Your Neighbour"
-	heroes["The Flash"] = "B Allen"
-	villains["Lex Luther"] = "Lex Luther"
-
-	superPets := map[int]string{1: "Krypto", 2: "Bat Hound", 3: "SomeOther"}
-	pf("Batman is %v\n", heroes["Batman"])
-	pf("Catperson is %v\n", heroes["Catperson"])
-	pf("SuperPet 1 : %v\n", superPets[1])
-
-	_, petExists := superPets[3]
-	pl("Is there a third superPet : ", petExists)
-
-	// structs
-
-	var tS customer
-	tS.name = "Tom Smith"
-	tS.address = "4 Main St"
-	tS.bal = 234.55
-
-	getCustomerInfo(tS)
-	updateCustAddress(&tS, "123 Nord Street, Town Central")
-	getCustomerInfo(tS)
-	tS.builtInAddressUpdate("666 Downtown Hell")
-	getCustomerInfo(tS)
-
-	sS := customer{"Sally Smith", "333 Death Road", 0.0}
-	pl("Name of new customer :", sS.name)
-
-	pl("-----------------------------")
-
-	rect1 := rectangle{10.0, 14.3}
-	pl("Rect Area : ", rect1.Area())
-
-	pl("-----------------------------")
-
-	con1 := contact{
-		"James",
-		"Wang",
-		"5555-66666"}
-
-	bus1 := business{
-		"ABC Plumbing",
-		"234 North Street",
-		con1}
-
-	bus1.info()
-
-	pl("-----------------------------")
-
-	var kitty Aniamal
-	kitty = Cat("Kitty")
-	kitty.AngrySound()
-
-	var kitty2 Cat = kitty.(Cat)
-	kitty2.Attack()
-	kitty2 = Cat("New Kitty")
-	pl("Cats Name :", kitty2.Name())
-	pl("kitty is of type :", typeOf(kitty))
-	pl("kitty2 is of type :", typeOf(kitty2))
+	useFunc(sumVals, 5, 8)
 }
