aaecadb2af3c3f03574ee5a3b1027b802bdf9dc9
Mariusz Borowiak <neutron.meme@proton.me>
2023-09-21 17:38:37 +0100

    Added commentary

--- simple-web-app/main.go
+++ simple-web-app/main.go
@@ -28,6 +28,7 @@ func write(writer http.ResponseWriter, msg string) {
 	errorCheck(err)
 }
 
+// Data fetch from a file
 func getStrings(fileName string) []string {
 	var lines []string
 	file, err := os.Open(fileName)
@@ -44,19 +45,28 @@ func getStrings(fileName string) []string {
 	return lines
 }
 
+// Pass an english hello to a http reponse to print in a page
 func englishHandler(writer http.ResponseWriter, request *http.Request) {
 	write(writer, "Hello Internet")
 }
 
+// Pass a polish hello to a http reponse to print in a page
 func polishHandler(writer http.ResponseWriter, request *http.Request) {
 	write(writer, "Siema Internet")
 }
 
+// Handler to fetch data, map to object and display on a page
 func interactHandler(writer http.ResponseWriter, request *http.Request) {
+
+	// Data fetch [this could be switched to a db persistance]
 	todoVals := getStrings("todos.txt")
+
+	// Log retrieved data to console
 	fmt.Printf("%#v\n", todoVals)
+
 	tmpl, err := template.ParseFiles("view.html")
 	errorCheck(err)
+
 	todos := ToDoList{
 		ToDoCount: len(todoVals),
 		ToDos:     todoVals,
@@ -65,19 +75,27 @@ func interactHandler(writer http.ResponseWriter, request *http.Request) {
 
 }
 
+// New todo route
 func newHandler(writer http.ResponseWriter, request *http.Request) {
 	tmpl, err := template.ParseFiles("new.html")
 	errorCheck(err)
 	err = tmpl.Execute(writer, nil)
 }
 
+// Handling POST request from a new todo view
 func createHandler(writer http.ResponseWriter, request *http.Request) {
+
+	// Value passed from the form
 	todo := request.FormValue("todo")
+
+	// Options for working with the data file
 	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
 	file, err := os.OpenFile("todos.txt", options, os.FileMode(0600))
 	errorCheck(err)
+
 	_, err = fmt.Fprintln(file, todo)
 	errorCheck(err)
+
 	err = file.Close()
 	http.Redirect(writer, request, "/interact", http.StatusFound)
 }
